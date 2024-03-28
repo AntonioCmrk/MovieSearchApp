1f6cbdd0bd7e4f9be7db8900af84b0c952e3f889
acmrk <acmrk@fina.hr>
2024-03-28 12:48:08 +0100

    fix: prikazivanje teksta na hover

--- movie-search-app/src/components/MovieCard.tsx
+++ movie-search-app/src/components/MovieCard.tsx
@@ -6,7 +6,11 @@ export const MovieCard = ({ movie }: MovieCardProps) => {
   return (
     <div className="group relative bg-slate-300 rounded-lg cursor-pointer hover:shadow-2xl hover:shadow-violet-200  ">
       <div className="group absolute inset-x-0 bottom-0 p-4 max-h-full  translate-y-full ease-in delay-5000 hidden group-hover:block group-hover:-translate-y-16 z-10 overflow-hidden mb-5 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]">
-        <div className="">{movie.overview}</div>
+        <div className="mb-5">
+          {movie.overview.length > 350
+            ? movie.overview.substring(0, 350) + "..."
+            : movie.overview}
+        </div>
         <div>Rating: {movie.vote_average.toFixed(1)}</div>
       </div>
       <img
--- movie-search-app/src/components/ShowCard.tsx
+++ movie-search-app/src/components/ShowCard.tsx
@@ -6,7 +6,11 @@ export const ShowCard = ({ show }: ShowCardProps) => {
   return (
     <div className="group relative bg-slate-300 rounded-lg cursor-pointer hover:shadow-2xl hover:shadow-violet-200  ">
       <div className="group absolute inset-x-0 bottom-0 p-4 max-h-full  translate-y-full ease-in delay-1000 hidden group-hover:block group-hover:-translate-y-16 z-10 overflow-hidden text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]">
-        <div className="mb-5">{show.overview}</div>
+        <div className="mb-5">
+          {show.overview.length > 400
+            ? show.overview.substring(0, 400) + "..."
+            : show.overview}
+        </div>
         <div>Rating: {show.vote_average.toFixed(1)}</div>
       </div>
       <img
